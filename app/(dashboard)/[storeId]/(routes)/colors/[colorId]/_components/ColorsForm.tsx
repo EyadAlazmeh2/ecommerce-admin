5a1499d5efdff11d4ef6c1619f9e793987f962f5
EyadAlazmeh2 <eyadoalazmeh@gmail.com>
2023-12-20 20:46:41 +0300

    Build the color page and connect with api

---
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/_components/ColorsForm.tsx
@@ -0,0 +1,168 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import Heading from "@/components/ui/heading";
+import { Input } from "@/components/ui/input";
+import AlertModal from "@/components/ui/modals/alert-modal";
+import { Separator } from "@/components/ui/separator";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Color } from "@prisma/client";
+import axios from "axios";
+import { Trash } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { z } from "zod";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  value: z.string().min(4).regex(/^#/, {
+    message: "String must be a valid hex code.",
+  }),
+});
+
+type ColorsFormValues = z.infer<typeof formSchema>;
+
+const ColorsForm = ({ initialData }: { initialData: Color | null }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const title = initialData ? "Edit color" : "Create color";
+  const description = initialData ? "Edit a color" : "Add a new color";
+  const toastMessage = initialData ? "Color updated." : "Color created.";
+  const action = initialData ? "Save changes" : "Create";
+
+  const form = useForm<ColorsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: "",
+      value: "",
+    },
+  });
+
+  const onSubmit = async (data: ColorsFormValues) => {
+    try {
+      setLoading(true);
+      initialData
+        ? await axios.patch(
+            `/api/${params.storeId}/colors/${params.colorId}`,
+            data
+          )
+        : await axios.post(`/api/${params.storeId}/colors`, data);
+      router.push(`/${params.storeId}/colors`);
+      router.refresh();
+      toast.success(toastMessage);
+    } catch (error) {
+      toast.error("Something went wrong.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/colors/${params.colorId}`);
+      router.push(`/${params.storeId}/colors`);
+      router.refresh();
+      toast.success("Color deleted.");
+    } catch (error) {
+      toast.error("Make sure you removed all products using this color first.");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+        {initialData && (
+          <Button
+            variant="destructive"
+            disabled={loading}
+            size="icon"
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Color name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="value"
+              render={({ field }) => (
+                <FormItem className="relative">
+                  <FormLabel>Value</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Color value"
+                      {...field}
+                    />
+                  </FormControl>
+                  <Input
+                    disabled={loading}
+                    className=" w-8 h-8 rounded-full overflow-hidden absolute border-none right-1 top-7 p-1"
+                    type="color"
+                    {...field}
+                  />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            {action}
+          </Button>
+        </form>
+      </Form>
+      <Separator />
+    </>
+  );
+};
+
+export default ColorsForm;
---
+++ app/api/[storeId]/colors/[colorId]/route.ts
@@ -0,0 +1,102 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { colorId: string } }
+) {
+  try {
+    if (!params.colorId)
+      return NextResponse.json("Color id is required", { status: 400 });
+
+    const color = await prismadb.color.findUnique({
+      where: {
+        id: params.colorId,
+      },
+    });
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log("[COLOR_GET]", error);
+    return NextResponse.json("Interal error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { storeId: string; colorId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await request.json();
+
+    const { name, value } = body;
+
+    if (!userId) return NextResponse.json("Unauthorized", { status: 401 });
+
+    if (!name) return NextResponse.json("Name is required", { status: 400 });
+
+    if (!value) return NextResponse.json("Value is required", { status: 400 });
+
+    if (!params.colorId)
+      return NextResponse.json("Size id is required", { status: 400 });
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId)
+      return new NextResponse("Unauthorized", { status: 403 });
+
+    const color = await prismadb.color.updateMany({
+      where: {
+        id: params.colorId,
+      },
+      data: {
+        name,
+        value,
+      },
+    });
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log("[COLOR_PATCH]", error);
+    return NextResponse.json("Interal error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { storeId: string; colorId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) return NextResponse.json("Unauthorized", { status: 401 });
+
+    if (!params.colorId)
+      return NextResponse.json("Color id is required", { status: 400 });
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId)
+      return new NextResponse("Unauthorized", { status: 403 });
+
+    const color = await prismadb.color.deleteMany({
+      where: {
+        id: params.colorId,
+      },
+    });
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log("[COLOR_DELETE]", error);
+    return NextResponse.json("Interal error", { status: 500 });
+  }
+}
